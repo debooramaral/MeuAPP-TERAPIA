5d0d78706f20e1a54c919acaee7adc25b6db0fb9
Débora Amaral <87581131+debooramaral@users.noreply.github.com>
2022-06-02 23:56:41 -0400

    pastas
    documentos com telas stack + tab navigation e somente stack. BASE, para possiveis reset/correções futuras

---
+++ src_TABNAV/paginas/Entrar/index.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, StatusBar } from 'react-native';
+import * as Animatable from 'react-native-animatable'
+import { useNavigation } from '@react-navigation/native'
+
+export default function Entrar() {
+
+  const navigation = useNavigation();
+
+ return (
+   <View style={styles.container}>
+     <StatusBar backgroundColor="#BF3467" barStyle="dark-content"/>
+      <Animatable.View animation="fadeInLeft" delay={1000} style={styles.containerHeader}>
+        <Text style={styles.message}>Bem-Vindo(a)</Text>
+      </Animatable.View>
+
+      <Animatable.View animation={"fadeInUp"} style={styles.containerForm}>
+        <Text style={styles.title}>E-MAIL</Text>
+        <TextInput
+          placeholder='Digite um e-mail'
+          style={styles.input}
+        />
+
+        <Text style={styles.title}>SENHA</Text>
+        <TextInput
+          placeholder='Digite a senha'
+          style={styles.input}
+        />
+
+        <TouchableOpacity 
+        style={styles.button}
+        onPress={() => navigation.navigate('Home')}
+        >
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+        style={styles.buttonRegister}
+        onPress={() => navigation.navigate('Cadastrar')}
+        >
+          <Text style={styles.registerText}>Não Possui uma Conta? CADASTRE-SE</Text>
+        </TouchableOpacity>
+      
+      </Animatable.View>    
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#BF3467',
+  },
+  containerHeader:{
+    marginTop: '15%',
+    marginBottom: '8%',
+  },
+  message:{
+    fontSize: 35,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  containerForm:{
+    backgroundColor:'#F2C4C4',
+    height: '45%',
+    width: '95%',
+    margin: '2.5%',
+    marginTop: '20%',
+    marginBottom: '8%',
+    borderRadius: 25,
+    alignItems: 'center',
+  },
+  title:{
+    fontSize: 20,
+    marginTop: 25,
+  },
+  input:{
+    borderBottomWidth: 1,
+    height: 40,
+    width: 300,
+    marginBottom: 12,
+    fontSize: 16,
+  },
+  button:{
+    backgroundColor: '#BF3467',
+    width: '50%',
+    borderRadius: 10,
+    paddingVertical: 8,
+    marginTop: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText:{
+    color: '#F2C4C4',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  buttonRegister:{
+    marginTop: 14,
+  },
+  registerText:{
+    color:'#a1a1a1'
+  }
+})
---
+++ src_TABNAV/rotas/index.js
@@ -0,0 +1,40 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import Inicio from '../paginas/Inicio'
+import Entrar from '../paginas/Entrar'
+import Cadastrar from '../paginas/Cadastrar'
+
+import Home from '../paginas/Home'
+
+const Stack = createNativeStackNavigator();
+
+export default function Rotas(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Inicio"
+                component={Inicio}
+                options={{headerShown: false}}
+            />
+
+            <Stack.Screen
+                name="Entrar"
+                component={Entrar}
+                options={{headerShown: false}}
+            />
+
+            <Stack.Screen
+                name="Cadastrar"
+                component={Cadastrar}
+                options={{headerShown: false}}
+            />
+
+            <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{headerShown: false}}
+            />
+        </Stack.Navigator>
+    )
+};
+
